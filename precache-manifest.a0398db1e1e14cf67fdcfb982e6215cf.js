0c9023b1f9f6f139dbe0da112dd546fb9fa71f3f
Farooque Reza <77841790+rezafarooque736@users.noreply.github.com>
2022-03-21 14:27:55 +0530

    Updates

---
+++ precache-manifest.a0398db1e1e14cf67fdcfb982e6215cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1280f6eaea55016af1f",
+    "url": "/textUtils-React/static/css/main.4fe6cd4b.chunk.css"
+  },
+  {
+    "revision": "f1280f6eaea55016af1f",
+    "url": "/textUtils-React/static/js/main.74b806c2.chunk.js"
+  },
+  {
+    "revision": "c6a1e4dc46d037d62ab7",
+    "url": "/textUtils-React/static/js/runtime~main.7b364d91.js"
+  },
+  {
+    "revision": "1a70ea3521e56972c9b6",
+    "url": "/textUtils-React/static/js/2.0dd74d0f.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/textUtils-React/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "b92839456f3410cd40e5a8f51011c15b",
+    "url": "/textUtils-React/index.html"
+  }
+];
